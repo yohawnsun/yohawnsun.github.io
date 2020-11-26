7bebec52248f5ccc2c47be9e041c7a78da3e5e96
HM\POJOH <pontus.johansson@hm.com>
2020-11-26 13:10:49 +0100

    sadasdsad

---
+++ rock_paper_scissor/script.js
@@ -0,0 +1,85 @@
+
+function returnResult(player, computer){
+
+    if (player === computer)
+        return "Draw!"
+
+    if (player==="r" && computer==="s" || player==="s" && computer==="p" || player==="p" && computer==="r")
+        return "Player won!"
+
+
+    return "Computer won!"
+
+
+}
+
+function setPic(choice){
+    let pic
+    switch (choice) {
+
+        case "r":
+            pic = "rock.png"
+            break;
+        case "p":
+                pic= "paper.png"
+                break;
+        default:
+            pic = "scissors.png"
+            break;
+    }
+
+    return pic
+}
+
+
+function translateComputerChoice(choice){
+    let translatedCPChoice
+
+    switch (choice) {
+        case 0:
+            translatedCPChoice = "r";
+            break;
+            case 1:
+                translatedCPChoice = "p";
+                break;
+        default:
+            translatedCPChoice = "s";
+            break;
+    }
+    return translatedCPChoice
+}
+
+
+
+
+function rockpaperschissors(choice){
+  
+    let computerChoice = translateComputerChoice(Math.floor(Math.random() * 3))
+    //translate choices
+    
+    console.log("computer: ",computerChoice)
+    console.log("player: ", choice)
+    //update pic
+    document.getElementById("computerpic").src = setPic(computerChoice)
+    document.getElementById("playerpic").src = setPic(choice)
+    
+    
+
+    //document.getElementById("result").innerText = "computer:" + fullDisplayChoice(translatedCPChoice) + "\r\n"+"Player:" + fullDisplayChoice(choice) + "\r\n"+ returnResult(choice, translatedCPChoice);
+
+    if(returnResult(choice, computerChoice)==="Player won!"){
+        console.log(document.getElementById("playerscore").innerText)
+        document.getElementById("playerscore").innerText= parseFloat(document.getElementById("playerscore").innerText)+1
+       
+    }
+
+    if(returnResult(choice, computerChoice)==="Computer won!"){
+        console.log(document.getElementById("playerscore").innerText)
+        document.getElementById("computerscore").innerText= parseFloat(document.getElementById("computerscore").innerText)+1
+       
+    }
+
+  
+    
+
+}
