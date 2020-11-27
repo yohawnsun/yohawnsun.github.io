3915da0e8aa043308a1ad41c3bb6c4ddd3bf6543
HM\POJOH <pontus.johansson@hm.com>
2020-11-27 16:31:59 +0100

    menu

--- script.js
+++ script.js
@@ -2,23 +2,14 @@ let playerCurrentScore=0
 let computerCurrentScore=0
 let playerName ="Player"
 
-
-
-
-
 function start(){
 
     playerName=document.getElementById("playername").value.length>0?document.getElementById("playername").value:"Player"
 
     document.getElementById("startdiv").remove()
-
     printScoreBoard("playerscore", playerName, playerCurrentScore)
     printScoreBoard("computerscore","Computer",computerCurrentScore)
-
-    //document.getElementById("startbutton").remove()
 }
-
-
 function whoWon(player, computer){
 
     if (player === computer)
@@ -27,23 +18,14 @@ function whoWon(player, computer){
     if (player==="r" && computer==="s" || player==="s" && computer==="p" || player==="p" && computer==="r")
         return "Player won!"
 
-
     return "Computer won!"
-
-
 }
-
-//removed two switch-cases
-
+//removed two switch-cases 2020-11-27
 function reset(){
     document.getElementById("computerpic").src = "unnamed.png"
-    
     printScoreBoard("playerscore", playerName, 0)
     printScoreBoard("computerscore","Computer",0)
-
 }
-
-
 //Changed 2020-11-27
 const hands = [
     {"name": "rock", short: "r", "image":"rock.png", num:0},
@@ -51,19 +33,15 @@ const hands = [
     {"name": "scissors", short: "s", "image":"scissors.png",num:2}
 ]
 
-
-
+//added 2020-11-26
 function printScoreBoard(name, nameText, score){
     document.getElementById(name).innerText = `${nameText}: ${score}`
 }
 
-
-
 function rockpaperschissors(choice){
     let random = Math.floor(Math.random() * 3)
     console.log(random)
-    let computerChoice = hands.find(o => o.num === random).short //Changed 2020-11-27
-    
+    let computerChoice = hands.find(o => o.num === random).short //Changed 2020-11-27 
 
     document.getElementById("computerpic").src = hands.find(o => o.short === computerChoice).image //Changed 2020-11-27
     if(whoWon(choice, computerChoice)==="Player won!"){
