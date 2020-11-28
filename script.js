414f93a079947c455b0367b6677cc743858b7445
HMPOJOH <72437604+HMPOJOH@users.noreply.github.com>
2020-11-28 13:10:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ let computerCurrentScore=0
 let playerName ="Player"
 
 function start(){
-
     playerName=document.getElementById("playername").value.length>0?document.getElementById("playername").value:"Player"
     document.getElementById("startdiv").remove()
     printScoreBoard()
 }
 function whoWon(player, computer){
-
     if (player === computer)
         return "Draw!"
-
     if (player==="r" && computer==="s" || player==="s" && computer==="p" || player==="p" && computer==="r")
+    {
+        playerCurrentScore++;
         return "Player won!"
+    }
 
+    computerCurrentScore++; 
     return "Computer won!"
 }
 //removed two switch-cases 2020-11-27
@@ -33,29 +34,14 @@ const hands = [
 
 //added 2020-11-26
 function printScoreBoard(){
-    
         document.getElementById("playerscore").innerText = `${playerName}: ${playerCurrentScore}`
-    
         document.getElementById("computerscore").innerText = `Computer: ${computerCurrentScore}`   
 }
-
-
-
-function rockpaperschissors(choice){
+function rockpaperschissors(playerChoice){
     let random = Math.floor(Math.random() * 3)
     console.log(random)
     let computerChoice = hands[random].short //Changed 2020-11-27 
-
     document.getElementById("computerpic").src = hands[random].image //Changed 2020-11-27 //.find(o => o.short === computerChoice)
-    if(whoWon(choice, computerChoice)==="Player won!"){
-        playerCurrentScore++;
-   
-    }
-
-    if(whoWon(choice, computerChoice)==="Computer won!"){
-        computerCurrentScore++;    
-    }
-
+    whoWon(playerChoice, computerChoice)
     printScoreBoard()
-
 }
